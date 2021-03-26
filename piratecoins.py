7f4745c16c49bfc429ff0cf856e996798bc3a6bf
Nebiyou <nebiyoulegesse@gmail.com>
2021-03-26 16:07:54 -0400

    blockchain code coppied

---
+++ piratecoins.py
@@ -0,0 +1,124 @@
+# Blockchain Framework
+
+import datetime
+import json
+import hashlib
+from flask import Flask, jsonify
+
+# Building a Blockchain
+
+class Blockchain:
+
+    # initiating  the Genesis Block
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = "0")
+
+    def create_block(self, proof, previous_hash):
+        block = {"index": len(self.chain) + 1,
+                 "time_stamp": str(datetime.datetime.now()),
+                 "proof": proof,
+                 "previous_hash": previous_hash}
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    # proof of work
+    # Has to be a problem that is hard to solve but easy to verify
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            # implementation of the SHA256 hashing function
+
+            hash_opperation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_opperation[:4] == "0000":
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block["previous_hash"] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block["proof"]
+            proof = block["proof"]
+
+            hash_opperation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_opperation[:4] != "0000":
+                return False
+            previous_block = block
+            block_index += 1
+
+        return True
+
+# Mining the Blockchain
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+# initialaze a Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+@app.route("/mine_block", methods = ["GET"])
+
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block["proof"]
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+
+    block = blockchain.create_block(proof, previous_hash)
+    response = {"message": "Congradulations!! you just mined a block!",
+                "index": block["index"],
+                "times_tamp": block["time_stamp"],
+                "proof": block["proof"],
+                "previous_hash": block["previous_hash"]}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+
+@app.route("/get_chain", methods = ["GET"])
+
+def get_chain():
+    response = {"chain" : blockchain.chain,
+                "length": len(blockchain.chain)}
+
+    return jsonify(response), 200
+
+@app.route("/is_valid", methods = ["GET"])
+
+#checking is the Blockchain is valid
+
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+
+    if is_valid:
+        response = {"message": "The Blockchain is Valid"}
+    else:
+        response = {"message": "Invalid Blockchain"}
+
+    return jsonify(response), 200
+
+
+
+
+#Running the app
+
+app.run(host = "0.0.0.0", port = "5000")
+
+
+
